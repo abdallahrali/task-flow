d52ac1f899799d919f331a6f19077dc6c1cbf42e
Hana M AbdelHamid <haniaimos6@gmail.com>
2026-03-18 22:35:25 +0200

    commit msg

--- install.sql
+++ install.sql
@@ -22,3 +22,6 @@ CREATE TABLE `tasks` (
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- random ah user, i honestly do not know
+INSERT INTO `tasks` (`id`, `user_id`, `title`, `descriptions`, `is_completed`, `created_at`) VALUES ('21321', '54654', 'sdfsdfs', 'sdfsdfsdfds', '0', CURRENT_TIMESTAMP);
